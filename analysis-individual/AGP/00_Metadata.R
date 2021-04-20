cd166bc1c21b7e131fe0c3ebbde1d364ef7cf6e0
salomecarcy <shalom.carcy@gmail.com>
2021-04-19 22:44:18 -0400

    AGP-Metadata: identify correct metadata table

--- analysis-individual/AGP/00_Metadata.R
+++ analysis-individual/AGP/00_Metadata.R
@@ -5,3 +5,38 @@
 ##########################
 
 
+
+####################
+# IMPORT LIBRARIES #
+####################
+
+library(tidyverse)
+
+path <- "~/Projects/IBS_Meta-analysis_16S"
+
+
+
+###################
+# IMPORT METADATA #
+###################
+
+# This csv file was downloaded from the SRA
+# with the accession number PRJEB11419
+sradf <- read.csv(file.path(path, "data/analysis-individual/AGP/SraRunTable.csv"))
+
+# This tsv file was downloaded from the figshare
+# link shared by the paper from McDonald et al., 2018
+paperdf <- read_tsv(file.path(path, "data/analysis-individual/AGP/correctedt2.tsv"))
+
+# Compare two tables
+dim(sradf) # 33,610 samples
+dim(paperdf) # 17,854 samples
+
+# The paper was published in 2018, so it is possible that more samples have been added on the SRA since
+table(str_sub(paperdf$collection_date, start=-4)) # latest sample from paper metadata table is 2017
+table(str_sub(sradf$ENA.FIRST.PUBLIC..run., end=4)) # latest sample from SRA metadata is 2021
+
+# => We will extract sample names from SRA metadata
+
+
+
