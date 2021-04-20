bbf601044d0b021c05230792d0669262d903a344
salomecarcy <shalom.carcy@gmail.com>
2021-04-20 13:50:45 -0400

    AGP-Metadata: remove columns of no interest

--- analysis-individual/AGP/00_Metadata.R
+++ analysis-individual/AGP/00_Metadata.R
@@ -39,6 +39,74 @@ table(str_sub(sradf$ENA.FIRST.PUBLIC..run., end=4)) # latest sample from SRA met
 # => We will extract sample Runs from SRA metadata
 
 
+###################
+# CLEANUP METADATA #
+###################
+
+# For simplicity purposes, we will keep only samples and columns of interest
+
+# Keep only 16S rRNA seq
+sradf <- sradf[sradf$target_gene..exp. == "16S rRNA",]
+
+# Remove columns that will not be of use
+skip_columns <- c("BioProject",
+                  "center_name..exp.",
+                  "Center.Name",
+                  "Consent",
+                  "ENA.FIRST.PUBLIC..run.",
+                  "LibrarySelection",
+                  "LibrarySource",
+                  "Platform",
+                  "ReleaseDate",
+                  "sequencing_meth..exp.",
+                  "SRA.Study",
+                  "well_description..exp.",
+                  "run_prefix..exp.",
+                  "run_date..exp.",
+                  "Bytes",
+                  "DATASTORE.provider",
+                  "DATASTORE.region",
+                  "Title",
+                  "sample_plate..exp.",
+                  "target_gene..exp.",
+                  "target_subfragment..exp.",
+                  "exercise_location",
+                  "cosmetics_frequency",
+                  "pool_frequency",
+                  "last_travel",
+                  "Elevation",
+                  "contraceptive",
+                  "LATITUDE",
+                  "LONGITUDE",
+                  "fermented_plant_frequency",
+                  "dominant_hand",
+                  "livingwith",
+                  "flu_vaccine_date",
+                  "types_of_plants",
+                  "consume_animal_products_abx",
+                  "last_move",
+                  "age_corrected",
+                  "primer_plate..exp.",
+                  "well_id..exp.",
+                  "roommates_in_study",
+                  "collection_time")
+sradf <- sradf %>%
+  select(-all_of(skip_columns))
+
+
+# To check for IBS: antibiotic_history, probiotic_frequency?, diabetes?, ibd, gluten (celiac disease),
+# thyroid?, autoimmune?, fungal_overgrowth, clinical_condition?, lung_disease?, liver_disease?, kidney_disease?, cdiff,
+# acne_medication?
+
+# Cleanup columns: bowel_movement_frequency, bowel_movement_quality
+
+# Samples to keep : SAMPLE_TYPE (Stool or stool)
+
+# Healthy: subset_healthy
+
+table(sradf$acne_medication)
+ 
+
 ###############
 # IBS SAMPLES #
 ###############
