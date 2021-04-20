0651cf66642cdb7ec426f43f0452c9d7862a7940
salomecarcy <shalom.carcy@gmail.com>
2021-04-19 22:25:56 -0400

    AGP-Metadata: init Rscript

---
+++ analysis-individual/AGP/00_Metadata.R
@@ -0,0 +1,7 @@
+##########################
+# Purpose: select IBS and healthy samples from American Gut Project fecal data
+# Date: April 2021
+# Author: Salomé Carcy
+##########################
+
+
