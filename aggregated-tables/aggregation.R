0efe5b5e86ea7f8cfa2ccd6ac6d9a948fde025a4
salomecarcy <shalom.carcy@gmail.com>
2021-04-21 12:30:36 -0400

    Aggregation: counts per aggregation level

--- aggregated-tables/aggregation.R
+++ aggregated-tables/aggregation.R
@@ -27,6 +27,39 @@ physeq.zhuang <- readRDS(file.path(path, "phyloseq-objects/physeq_zhuang.rds"))
 
 
 
+####################
+# TAXA AGGREGATION #
+####################
+
+#_________________________
+# Check that level of aggregation changes the total count at higher taxonomic levels
+test.genus <- physeq.fukui %>%
+  tax_glom(taxrank = "Genus") %>% # Genus aggregation
+  psmelt() %>%
+  group_by(Phylum) %>%
+  summarise(Abundance=sum(Abundance)) %>%
+  rename(genus_agg = Abundance)
+
+test.phylum <- physeq.fukui %>%
+  tax_glom(taxrank = "Phylum") %>% # Phylum aggregation
+  psmelt() %>%
+  group_by(Phylum) %>%
+  summarise(Abundance=sum(Abundance)) %>%
+  rename(phylum_agg = Abundance)
+
+# Compare the total count per phylum after genus/phylum aggregation
+test.genus %>%
+  right_join(test.phylum, by="Phylum")
+# Different total count (some phyla have lower total count with the genus aggregation)
+
+
+
+
+
+
+
+
+
 
 
 
