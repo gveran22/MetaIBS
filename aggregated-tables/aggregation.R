c3e0d40bbfe8c945f8bada33c219b50844dbc485
salomecarcy <shalom.carcy@gmail.com>
2021-04-21 12:01:32 -0400

    Aggregation: import data

--- aggregated-tables/aggregation.R
+++ aggregated-tables/aggregation.R
@@ -4,3 +4,30 @@
 # Author: Salomé Carcy
 ##########################
 
+
+##########
+# IMPORT #
+##########
+
+# Libraries
+library(phyloseq)
+library(tidyverse)
+
+# Data
+path <- "~/Projects/IBS_Meta-analysis_16S"
+physeq.fukui <- readRDS(file.path(path, "phyloseq-objects/physeq_fukui.rds"))
+physeq.hugerth <- readRDS(file.path(path, "phyloseq-objects/physeq_hugerth.rds"))
+physeq.labus <- readRDS(file.path(path, "phyloseq-objects/physeq_labus.rds"))
+physeq.lopresti <- readRDS(file.path(path, "phyloseq-objects/physeq_lopresti.rds"))
+physeq.nagel <- readRDS(file.path(path, "phyloseq-objects/physeq_nagel.rds"))
+physeq.pozuelo <- readRDS(file.path(path, "phyloseq-objects/physeq_pozuelo.rds"))
+physeq.zeber <- readRDS(file.path(path, "phyloseq-objects/physeq_zeber.rds"))
+physeq.zhu <- readRDS(file.path(path, "phyloseq-objects/physeq_zhu.rds"))
+physeq.zhuang <- readRDS(file.path(path, "phyloseq-objects/physeq_zhuang.rds"))
+
+
+
+
+
+
+
