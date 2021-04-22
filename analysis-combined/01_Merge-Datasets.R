997f1af8732e62f114a1a07205b265a0e0702df7
salomecarcy <shalom.carcy@gmail.com>
2021-04-22 15:03:56 -0400

    Common-ASVs: start script

---
+++ analysis-combined/01_Merge-Datasets.R
@@ -0,0 +1,35 @@
+##########################
+# Purpose: merge phyloseq object and merge ASVs
+# Date: April 2021
+# Author: Salomé Carcy
+##########################
+
+
+##########
+# IMPORT #
+##########
+
+# Libraries
+library(tidyverse)
+library(phyloseq)
+
+# Data
+path <- "~/Projects/IBS_Meta-analysis_16S"
+physeq.fukui <- readRDS(file.path(path, "phyloseq-objects/physeq_fukui.rds"))
+physeq.hugerth <- readRDS(file.path(path, "phyloseq-objects/physeq_hugerth.rds"))
+physeq.labus <- readRDS(file.path(path, "phyloseq-objects/physeq_labus.rds"))
+physeq.lopresti <- readRDS(file.path(path, "phyloseq-objects/physeq_lopresti.rds"))
+physeq.nagel <- readRDS(file.path(path, "phyloseq-objects/physeq_nagel.rds"))
+physeq.pozuelo <- readRDS(file.path(path, "phyloseq-objects/physeq_pozuelo.rds"))
+physeq.zeber <- readRDS(file.path(path, "phyloseq-objects/physeq_zeber.rds"))
+physeq.zhu <- readRDS(file.path(path, "phyloseq-objects/physeq_zhu.rds"))
+physeq.zhuang <- readRDS(file.path(path, "phyloseq-objects/physeq_zhuang.rds"))
+
+
+#####################
+# CHECK COMMON ASVs #
+#####################
+
+# Get ASVs per dataset
+
+
