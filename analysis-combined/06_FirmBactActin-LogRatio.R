832ca537299d24de8417d539fc0d45c0a060018b
salomecarcy <shalom.carcy@gmail.com>
2021-08-21 16:08:45 -0400

    FA-logRatio: plot

--- analysis-combined/06_FirmBactActin-LogRatio.R
+++ analysis-combined/06_FirmBactActin-LogRatio.R
@@ -329,23 +329,153 @@ ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin
 
 
 
-##________________________________________
-## STATISTICS
-
-
-
-
 ##############################################
 # PLOT FIRMICUTES/ACTINOBACTERIOTA LOG RATIO #
 ##############################################
 
-##________________________________________
-## BUILD DATAFRAME
-# Extract abundance of Bacteroidota and Firmicutes
+# Plot Firm/Actino ratio PER DATASET, HEALTHY/IBS, with FACET_WRAP
+ggplot(ratio.df %>% filter(sample_type == "stool", Collection=="1st"),
+       aes(x = host_disease, y = LogRatio_FirmAct,
+           fill=factor(author, levels=author.order)))+
+  facet_wrap(~factor(author, levels = author.order), scales="fixed")+
+  geom_violin()+
+  geom_jitter(width = 0.1, size = 0.3)+
+  theme_classic()+
+  scale_fill_brewer(palette="Set3")+
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
+        axis.line = element_line(arrow = arrow(length = unit(0.1, "inches"))),
+        legend.position="None")+
+  labs(x = '', y = "Log2(Firmicutes/Actinobacteriota)")
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_01.jpg", width=8, height=6)
 
 
+# Plot Firm/Actino ratio PER DATASET, HEALTHY/IBS, with BOXPLOTS
+ggplot(ratio.df %>% filter(Collection=="1st"),
+       aes(x = author_disease, y = LogRatio_FirmAct,
+           color=factor(author, levels = author.order)))+
+  geom_boxplot(outlier.shape = NA, width = 0.4, lwd=1)+
+  geom_jitter(width = 0.1, size = 0.3)+
+  theme_classic()+
+  scale_x_discrete(breaks=author_disease.order, labels= rep(c("Healthy", "IBS"), times = 12))+
+  theme(axis.text.x = element_text(angle = 45, hjust=1),
+        axis.line = element_line(arrow = arrow(length = unit(0.1, "inches"))))+
+  labs(x = '', y = "Log2(Firmicutes/Actinobacteriota)", color="author", title="All samples (sigmoid & stool)")
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_02.jpg", width=12, height=5)
 
 
+# Plot Firm/Actino ratio PER SEQUENCING TECH, HEALTHY/IBS
+ggplot(ratio.df %>% filter(sample_type == "stool", Collection=="1st"),
+       aes(x = sequencing_tech, y = LogRatio_FirmAct,
+           fill=host_disease))+
+  geom_violin(alpha=0.5)+
+  geom_boxplot(aes(group=interaction(host_disease,sequencing_tech)), position = position_dodge(0.9),
+               lwd = 1, width = 0.2, fill = "white", outlier.shape=NA)+
+  theme_classic()+
+  scale_fill_manual(values = c("blue", "red"))+
+  theme(axis.text.x = element_text(size=15, color="black", angle=35, hjust=1),
+        axis.title = element_text(size=15),
+        axis.line = element_line(arrow = arrow(length = unit(0.1, "inches"))))+
+  labs(x = '', y = "Log2(Firmicutes/Actinobacteriota)", fill="Disease")
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_03.jpg", width=12, height=5)
+
+
+# Plot Firm/Actino ratio POOLED together (HEALTHY/IBS)
+ggplot(ratio.df %>% filter(sample_type == "stool", Collection=="1st"),
+       aes(x = host_disease, y = LogRatio_FirmAct, fill=host_disease))+
+  geom_violin(alpha = 0.5)+
+  geom_boxplot(lwd = 1, width = 0.2, fill = "white")+
+  # geom_jitter(width = 0.1, size = 0.3)+
+  scale_fill_manual(values=c("blue", "red"))+
+  labs(x = '', y = "Log2(Firmicutes/Actinobacteriota)")+
+  theme_classic()+
+  theme(axis.text = element_text(size=15, color="black"),
+        axis.title = element_text(size=15),
+        axis.line = element_line(arrow = arrow(length = unit(0.1, "inches"))),
+        legend.position="None")
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_04.jpg", width=5, height=4)
+
+
+# Plot Firm/Actino ratio POOLED together by IBS SUBTYPE
+ggplot(ratio.df %>% filter(sample_type == "stool", Collection=="1st",
+                           host_subtype != "IBS-unspecified", host_subtype != "HC-unknown",
+                           author %in% c("Labus", "LoPresti", "Liu", "Pozuelo", "Mars", "Zhuang", "Nagel", "Zeber-Lubecka")),
+       aes(x = host_subtype, y = LogRatio_FirmAct))+
+  geom_boxplot(lwd = 1, width=0.3, outlier.shape=NA, fill = "white")+
+  geom_jitter(aes(color = factor(author, levels = author.order)), width = 0.05, size=0.6)+
+  labs(title="Stool samples only",
+       x = '', y = "Log2(Firmicutes/Actinobacteriota)",
+       color="author")+
+  theme_classic()+
+  theme(axis.text = element_text(size=10, color="black"),
+        axis.title = element_text(size=10))
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_05.jpg", width=5, height=4)
+
+
+# Plot Firm/Actino ratio POOLED together by IBS SUBTYPE, with FACET_WRAP
+ggplot(ratio.df %>% filter(Collection=="1st",
+                           host_subtype != "IBS-unspecified", host_subtype != "HC-unknown",
+                           author %in% c("Labus", "LoPresti", "Liu", "Pozuelo", "Mars", "Zhuang", "Nagel", "Zeber-Lubecka")),
+       aes(x = host_subtype, y = LogRatio_FirmAct))+
+  geom_violin()+
+  facet_wrap(~author, scales="fixed", ncol=2)+
+  #ylim(c(0,15))+
+  #geom_boxplot(lwd = 1, width=0.3, outlier.shape=NA, fill = "white")+
+  geom_jitter(aes(color = factor(author, levels = author.order)), width = 0.05, size=2, alpha=0.8)+
+  labs(title="All sample types (stool & sigmoid)",
+       x = '', y = "Log2(Firmicutes/Actinobacteriota)",
+       color="author")+
+  theme_classic()+
+  theme(axis.text = element_text(size=10, color="black"),
+        axis.title = element_text(size=10))
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_06.jpg", width=6, height=8)
+
+
+# LIU BRISTOL STOOL SCALE (Firmicutes/Actinobacteriota)
+ggplot(ratio.df %>% filter(author == "Liu"),
+       aes(x = Bristol, y = LogRatio_FirmAct))+
+  # geom_violin(alpha = 0.5)+
+  geom_boxplot(aes(group=Bristol), lwd = 1, width=0.3, outlier.shape=NA, fill = "white")+
+  geom_jitter(aes(color=host_disease), alpha=0.5, width = 0.05, size=3)+
+  scale_color_manual(name = "disease", values=c("blue", "red"))+
+  labs(title="Liu dataset (2020)",
+       x = 'Bristol stool scale', y = "Log2(Firmicutes/Actinobacteriota)")+
+  theme_classic()+
+  theme(axis.text = element_text(size=15, color="black"),
+        axis.title = element_text(size=15),
+        axis.line = element_line(arrow = arrow(length = unit(0.1, "inches"))))
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_07.jpg", width=6, height=6)
+
+
+# AGP bowel_movement_quality
+ggplot(ratio.df %>% filter(author == "AGP" & host_subtype!="HC-unknown" & host_subtype!="IBS-unspecified") %>%
+         mutate(bowel_movement_quality=factor(bowel_movement_quality, levels=c("Normal", "Constipated", "Diarrhea"))),
+       aes(x = bowel_movement_quality, y = LogRatio_FirmAct))+
+  #geom_violin(alpha = 0.5)+
+  geom_boxplot(aes(group=bowel_movement_quality), lwd = 1, width=0.3, outlier.shape=NA, fill = "white")+
+  geom_jitter(aes(color=host_disease), alpha=0.5, width = 0.05, size=2)+
+  scale_color_manual(name = "disease", values=c("blue", "red"))+
+  labs(title="AGP dataset (2021)",
+       x = 'Bowel movement quality', y = "Log2(Firmicutes/Actinobacteriota)")+
+  theme_classic()+
+  theme(axis.text = element_text(size=12, color="black"),
+        axis.title = element_text(size=15))
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_08.jpg", width=6, height=6)
+
+
+# AGP bowel_movement_frequency
+ggplot(ratio.df %>% filter(author == "AGP" & bowel_movement_frequency %in% freq.order) %>%
+         mutate(bowel_movement_frequency=factor(bowel_movement_frequency, levels=freq.order)),
+       aes(x = bowel_movement_frequency, y = LogRatio_FirmAct))+
+  #geom_violin(alpha = 0.5)+
+  geom_boxplot(aes(group=bowel_movement_frequency), lwd = 1, width=0.3, outlier.shape=NA, fill = "white")+
+  geom_jitter(aes(color=factor(bowel_movement_quality, levels=c("Normal", "Constipated", "Diarrhea"))), alpha=0.5, width = 0.05, size=2)+
+  scale_color_manual(values=c("#6699FF", "#CC0000","#CC33FF"))+
+  labs(title="AGP dataset (2021)", color="Bowel mvt quality",
+       x = 'Bowel movement frequency (per day)', y = "Log2(Firmicutes/Actinobacteriota)")+
+  theme_classic()+
+  theme(axis.text = element_text(size=12, color="black", angle=40, hjust=1),
+        axis.title = element_text(size=15))
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/analysis-combined/06_FirmBactActin-LogRatio/firm_actino_09.jpg", width=6, height=6)
 
 
 
