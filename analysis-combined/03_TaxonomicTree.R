af76d2a5c57df9b2aa2680f44ed941fb420f1593
salomecarcy <shalom.carcy@gmail.com>
2021-09-13 20:21:37 -0400

    trying out different methods to get taxonomic tree

--- analysis-combined/03_TaxonomicTree.R
+++ analysis-combined/03_TaxonomicTree.R
@@ -15,7 +15,6 @@
 library(phyloseq)
 library(ggplot2)
 library(tidyverse)
-library(trac)
 
 # Data
 path.phy <- "~/Projects/IBS_Meta-analysis_16S/phyloseq-objects"
@@ -35,10 +34,86 @@ physeq.zeber <- readRDS(file.path(path.phy, "physeq_zeber.rds"))
 
 
 
-#################
-# TESTING STUFF #
-#################
+#############################
+# TESTING STUFF WITH PHYLOG #
+#############################
 
+# With MicrobiotaProcess
+library(MicrobiotaProcess)
+library(ggtree)
+
+# data("kostic2012crc")
+# taxa <- phyloseq::tax_table(kostic2012crc)
+# tree <- as.treedata(taxa)
+# ggtree(tree, layout="circular", size=0.2) +
+#   geom_tiplab(size=1)
+
+taxa <- tax_table(physeq.labus)
+tree <- as.treedata(taxa)
+
+ggtree(tree, layout='circular') + 
+  aes(color="Phylum")
+
+
+# With taxo2phylog
+library(ade4)
+data(taxo.eg)
+taxo.eg[[1]]
+as.taxo(taxo.eg[[1]])
+
+
+tax <- as.data.frame(physeq.labus@tax_table@.Data, stringsAsFactors = TRUE)
+tax$Kingdom <- NULL
+tax$Species <- NULL
+as.taxo(tax[,1:2]) # problem
+
+tax.phy <- taxo2phylog(tax)
+
+
+
+
+# With as.phylo
+library(treeio)
+library(ggtree)
+data("GNI2014", package="treemap")
+n <- GNI2014[, c(3,1)]
+n[,1] <- as.character(n[,1])
+n[,1] <- gsub("\\s\\(.*\\)", "", n[,1]) # n is a dataframe with continent and country code
+
+w <- cbind("World", as.character(unique(n[,1])))
+colnames(w) <- colnames(n) # w is a dataframe with world & continent (but same column name?...)
+
+edgelist <- rbind(n, w)
+
+y <- as.phylo(edgelist)
+ggtree(y, layout='circular') %<+% GNI2014 + 
+  aes(color=continent) + geom_tippoint(aes(size=population), alpha=.6) + 
+  geom_tiplab(aes(label=country), offset=.1) +
+  theme(plot.margin=margin(60,60,60,60))
+
+# try with labus
+tax <- as.data.frame(physeq.labus@tax_table@.Data, stringsAsFactors = FALSE, row.names=NULL)
+
+test1 <- unique( tax[,2:3] )
+rownames(test1) <- NULL
+test1[is.na(test1$Class),"Class"] <- paste0("c__", 1:2)
+test2 <- cbind("Bacteria", as.character(unique(tax[,2])))
+colnames(test2) <- colnames(test1)
+
+test <- rbind(test1, test2)
+testplot <- as.phylo(test)
+
+ggtree(testplot, layout='circular') %<+% tax + 
+  aes(color=Phylum) +
+  geom_tippoint(size=1, alpha=.6) +
+  geom_tiplab(aes(label="Class"), offset=.1)
+
+
+###########################
+# TESTING STUFF WITH TRAC #
+###########################
+
+library(trac)
 
 tax <- physeq.labus@tax_table@.Data
 # Add a "Life" column (rank0)
@@ -74,6 +149,8 @@ for (i in 2:9) {
 }
 tax <- as.data.frame(tax, stringsAsFactors = TRUE)
 
+
+###### DOESN'T WORK (R SESSION ABORTS) ##########
 # form phylo object:
 # tree1 <- tax_table_to_phylo(~Rank0/Rank1/Rank2/Rank3/Rank4/Rank5/Rank6/Rank7/ASV,
 #                             data = tax, collapse = TRUE)
