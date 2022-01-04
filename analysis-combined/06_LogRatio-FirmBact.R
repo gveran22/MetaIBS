c258c8b286d6f02e221d25d35b8027b96bc24354
salomecarcy <shalom.carcy@gmail.com>
2022-01-03 20:08:55 -0500

    Analysis-combined: log ratio FB figure

--- analysis-combined/06_LogRatio-FirmBact.R
+++ analysis-combined/06_LogRatio-FirmBact.R
@@ -87,28 +87,12 @@ firmi <- phylumTable %>%
   rename(Firmicutes = Abundance) %>%
   select(-Phylum)
 
-actino <- phylumTable %>%
-  filter(Phylum == "Actinobacteriota") %>%
-  select(all_of(relevant.covariates)) %>%
-  rename(Actinobacteriota = Abundance) %>%
-  select(-Phylum)
-
-proteo <- phylumTable %>%
-  filter(Phylum == "Proteobacteria") %>%
-  select(all_of(relevant.covariates)) %>%
-  rename(Proteobacteria = Abundance) %>%
-  select(-Phylum)
-
 
 # Check if there are any count 0 (would prevent from calculating ratio)
 table(bacter$Bacteroidota == 0) # 17
 table(firmi$Firmicutes == 0) # 3
-table(actino$Actinobacteriota == 0) # 148
-table(proteo$Proteobacteria == 0) # 148
 min(bacter[bacter$Bacteroidota > 0, "Bacteroidota"]) # 4
 min(firmi[firmi$Firmicutes > 0, "Firmicutes"]) # 105
-min(actino[actino$Actinobacteriota > 0, "Actinobacteriota"]) # 2
-min(proteo[proteo$Proteobacteria > 0, "Proteobacteria"]) # 2
 
 
 # Sanity check (should all have 2,576 samples)
@@ -124,21 +108,12 @@ common.columns <- c("Sample", "host_disease", "host_subtype", "sample_type", "Co
                     "host_age", "host_bmi")
 
 ratio.df <- left_join(x=bacter, y=firmi, by=common.columns) %>%
-  left_join(actino, by=common.columns) %>%
-  left_join(proteo, by=common.columns) %>%
   relocate(Bacteroidota, .before=Firmicutes) %>%
   # Add 0.5 pseudocounts for the few 0 values
   mutate(Bacteroidota=replace(Bacteroidota, Bacteroidota==0, 0.5),
-         Firmicutes=replace(Firmicutes, Firmicutes==0, 0.5),
-         Actinobacteriota=replace(Actinobacteriota, Actinobacteriota==0, 0.5),
-         Proteobacteria=replace(Proteobacteria, Proteobacteria==0, 0.5)) %>%
+         Firmicutes=replace(Firmicutes, Firmicutes==0, 0.5)) %>%
   # Compute log ratios
-  mutate(LogRatio_FirmBact = log2(Firmicutes/Bacteroidota),
-         LogRatio_FirmAct = log2(Firmicutes/Actinobacteriota),
-         LogRatio_BactAct = log2(Bacteroidota/Actinobacteriota),
-         LogRatio_FirmProt = log2(Firmicutes/Proteobacteria),
-         LogRatio_BactProt = log2(Bacteroidota/Proteobacteria),
-         LogRatio_ActProt = log2(Actinobacteriota/Proteobacteria)) %>%
+  mutate(LogRatio_FirmBact = log2(Firmicutes/Bacteroidota)) %>%
   # Add column author_disease
   mutate(author_disease=paste0(author, sep="_", host_disease)) %>%
   # Add IBS subtype to AGP
@@ -177,7 +152,7 @@ ratio.df$sequencing_tech <- factor(ratio.df$sequencing_tech, levels=seqtech.orde
 ####################################################
 
 # Plot Firm/Bact ratio for fecal samples
-ggplot(ratio.df %>% filter(Collection=="1st" & sample_type=="stool"),
+a <- ggplot(ratio.df %>% filter(Collection=="1st" & sample_type=="stool"),
        aes(x = author, y = LogRatio_FirmBact, fill=host_disease))+
   # facet_wrap(~author, nrow=1, strip.position="bottom")+
   geom_boxplot(position=position_dodge(width=0.75), outlier.shape = NA, width = 0.4, lwd=0.5, alpha=0.2)+
@@ -187,12 +162,12 @@ ggplot(ratio.df %>% filter(Collection=="1st" & sample_type=="stool"),
   theme_cowplot()+
   # scale_x_discrete(breaks=author_disease.order, labels= rep(c("Healthy", "IBS"), times = 12))+
   theme(axis.text.x = element_text(angle = 45, color="black", hjust=1))+
-  labs(x = '', y = "Log2(Firmicutes/Bacteroidota)", fill="", title="All fecal samples")
+  labs(x = '', y = expression(Log[2](Firmicutes/Bacteroidota)), fill="", title="Stool samples")
 ggsave("~/Projects/IBS_Meta-analysis_16S/data/plots_paper/firm_bact_01.jpg", width=10, height=5)
 
 
-# Plot Firm/Bact ratio for fecal samples
-ggplot(ratio.df %>% filter(Collection=="1st" & sample_type=="sigmoid"),
+# Plot Firm/Bact ratio for sigmoid samples
+b <- ggplot(ratio.df %>% filter(Collection=="1st" & sample_type=="sigmoid"),
        aes(x = author, y = LogRatio_FirmBact, fill=host_disease))+
   # facet_wrap(~author, nrow=1, strip.position="bottom")+
   geom_boxplot(position=position_dodge(width=0.75), outlier.shape = NA, width = 0.4, lwd=0.5, alpha=0.2)+
@@ -202,14 +177,71 @@ ggplot(ratio.df %>% filter(Collection=="1st" & sample_type=="sigmoid"),
   theme_cowplot()+
   # scale_x_discrete(breaks=author_disease.order, labels= rep(c("Healthy", "IBS"), times = 12))+
   theme(axis.text.x = element_text(angle = 45, color="black", hjust=1))+
-  labs(x = '', y = "Log2(Firmicutes/Bacteroidota)", fill="", title="All sigmoid samples")
+  labs(x = '', y =  expression(Log[2](Firmicutes/Bacteroidota)), fill="", title="Sigmoid samples")
 ggsave("~/Projects/IBS_Meta-analysis_16S/data/plots_paper/firm_bact_02.jpg", width=5, height=5)
 
 
+# Plot Firm/Bact ratio per collection time point
+c <- ggplot(data = ratio.df %>%
+                group_by(author) %>%
+                filter(n_distinct(Collection)>1) %>%
+                ungroup() %>%
+                mutate(Collection=replace(Collection, Collection=="1st", "t0"),
+                       Collection=replace(Collection, Collection=="2nd" & author=="Mars", "t0+6mo"),
+                       Collection=replace(Collection, Collection=="2nd" & author=="Pozuelo", "t0+1mo")) %>%
+                mutate(author=recode(author, "Mars"="Mars (sigmoid)"),
+                       author=recode(author, "Pozuelo"="Pozuelo (stool)")),
+       aes(x = Collection, y = LogRatio_FirmBact, fill=host_disease))+
+  facet_wrap(~author, nrow=1, strip.position="top", scales="free_x")+
+  geom_boxplot(position=position_dodge(width=0.75), outlier.shape = NA, width = 0.4, lwd=0.5, alpha=0.2)+
+  geom_point(position=position_jitterdodge(dodge.width=0.75, jitter.width=0.1), aes(color=host_disease), size=0.2)+
+  scale_color_manual(values=c("#3182bd", "#de2d26"), guide="none")+
+  scale_fill_manual(values=c("#3182bd", "#de2d26"))+
+  theme_cowplot()+
+  # scale_x_discrete(breaks=author_disease.order, labels= rep(c("Healthy", "IBS"), times = 12))+
+  theme(axis.text.x = element_text(angle = 45, color="black", hjust=1))+
+  labs(x = '', y = expression(Log[2](Firmicutes/Bacteroidota)), fill="", title="Collection time point")
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/plots_paper/firm_bact_03.jpg", width=6, height=5)
 
 
+# Plot Firm/Bact ratio per IBS subtype
+d <- ggplot(
+    #+++++++++
+    # Keep only data of interest
+    data = ratio.df %>% filter(Collection=="1st",
+                               host_subtype!="IBS-unspecified", host_subtype!="HC-unknown",
+                               author %in% c("Labus", "LoPresti", "Liu", "Pozuelo", "Mars", "Zhuang", "Nagel", "Zeber-Lubecka"),
+                               !(author=="LoPresti" & sample_type=="sigmoid") # remove the (very) few sigmoid samples of LoPresti
+                               ) %>%
+                        mutate(author=recode(author, "Mars"="Mars (sigmoid)")) %>%
+                        mutate(host_subtype=replace(host_subtype, host_subtype=="HC", "Healthy")),
+    aes(x = host_subtype, y = LogRatio_FirmBact, fill=host_subtype))+
+    #+++++++++
+  facet_wrap(~author, scales="fixed", ncol=1, strip.position="right")+
+  # facet_grid(author~sample_type)+
+  #ylim(c(-3,10))+
+  # geom_violin(lwd=0.5, alpha=0.2)+
+  geom_boxplot(lwd=0.5, width=0.4, outlier.shape=NA, alpha=0.2)+
+  geom_jitter(aes(color=host_subtype), width=0.05, size=0.5)+
+  scale_color_manual(values=c("#3182bd", "#a50f15", "#fcae91", "#fb6a4a"), guide="none")+
+  scale_fill_manual(values=c("#3182bd", "#a50f15", "#fcae91", "#fb6a4a"), guide="none")+
+  # THEME
+  theme_cowplot()+
+  theme(strip.text.y = element_text(angle=0, hjust=0),
+        strip.background = element_blank())+
+  labs(title="IBS subtypes", x = '', y =  expression(Log[2](Firmicutes/Bacteroidota)))
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/plots_paper/firm_bact_04.jpg", width=5, height=9)
 
 
+# Combine plots
+ggdraw() +
+  draw_plot(a, x = 0, y = .5, width = .65, height = .5) +
+  draw_plot(b, x = 0, y = 0, width = .30, height = .5) +
+  draw_plot(c, x = 0.30, y = 0, width = .35, height = .5) +
+  draw_plot(d, x = 0.65, y = 0, width = 0.35, height = 1) +
+  draw_plot_label(label = c("A", "B", "C", "D"), size = 15,
+                  x = c(0, 0, 0.30, 0.65), y = c(1, 0.5, 0.5, 1))
+ggsave("~/Projects/IBS_Meta-analysis_16S/data/plots_paper/firm_bact_05.jpg", width=15, height=10)
 
 
 
