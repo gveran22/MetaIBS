3ab57f5d98e21a0b0e2e988e0b6402b36ec6138a
salomecarcy <shalom.carcy@gmail.com>
2022-03-19 13:50:07 -0400

    Agg-tables: update README

--- analysis-combined/01_Common-ASVs.R
+++ analysis-combined/01_Common-ASVs.R
@@ -18,20 +18,19 @@ library(cowplot)
 
 # Data
 path.phy <- "~/Projects/IBS_Meta-analysis_16S/data/analysis-individual/CLUSTER/PhyloTree/input"
-physeq.ringel <- readRDS(file.path(path.phy, "physeq_ringel.rds"))
-physeq.labus <- readRDS(file.path(path.phy, "physeq_labus.rds"))
+physeq.ringel   <- readRDS(file.path(path.phy, "physeq_ringel.rds"))
+physeq.labus    <- readRDS(file.path(path.phy, "physeq_labus.rds"))
 physeq.lopresti <- readRDS(file.path(path.phy, "physeq_lopresti.rds"))
-physeq.pozuelo <- readRDS(file.path(path.phy, "physeq_pozuelo.rds"))
-physeq.zhuang <- readRDS(file.path(path.phy, "physeq_zhuang.rds"))
-physeq.zhu <- readRDS(file.path(path.phy, "physeq_zhu.rds"))
-physeq.hugerth <- readRDS(file.path(path.phy, "physeq_hugerth.rds"))
-physeq.fukui <- readRDS(file.path(path.phy, "physeq_fukui.rds"))
-physeq.mars <- readRDS(file.path(path.phy, "physeq_mars.rds"))
-physeq.liu <- readRDS(file.path(path.phy, "physeq_liu.rds"))
-physeq.agp <- readRDS(file.path(path.phy, "physeq_agp.rds"))
-physeq.nagel <- readRDS(file.path(path.phy, "physeq_nagel.rds"))
-physeq.zeber <- readRDS(file.path(path.phy, "physeq_zeber.rds"))
-
+physeq.pozuelo  <- readRDS(file.path(path.phy, "physeq_pozuelo.rds"))
+physeq.zhuang   <- readRDS(file.path(path.phy, "physeq_zhuang.rds"))
+physeq.zhu      <- readRDS(file.path(path.phy, "physeq_zhu.rds"))
+physeq.hugerth  <- readRDS(file.path(path.phy, "physeq_hugerth.rds"))
+physeq.fukui    <- readRDS(file.path(path.phy, "physeq_fukui.rds"))
+physeq.mars     <- readRDS(file.path(path.phy, "physeq_mars.rds"))
+physeq.liu      <- readRDS(file.path(path.phy, "physeq_liu.rds"))
+physeq.agp      <- readRDS(file.path(path.phy, "physeq_agp.rds"))
+physeq.nagel    <- readRDS(file.path(path.phy, "physeq_nagel.rds"))
+physeq.zeber    <- readRDS(file.path(path.phy, "physeq_zeber.rds"))
 
 
 
