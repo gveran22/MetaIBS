6ef49a3e0c53414c631545a4b719d22a80689b25
salomecarcy <shalom.carcy@gmail.com>
2023-03-10 09:04:40 -0500

    LoPresti: download samples folder

---
+++ scripts/analysis-individual/LoPresti-2019/download-LoPresti-samples/download_fastq_lopresti.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Purpose: download LoPresti dataset
+#Author: Salome Carcy March.2021
+#--------------------------------------
+
+INPUT=list_files_lopresti.txt
+
+for FILE in $(cat list_files.txt )
+do
+	echo "Run: $FILE"
+	#~/sra-toolkit/fastq-dump --origfmt -X 2 -Z $FILE
+	~/sra-toolkit/fastq-dump --origfmt --gzip $FILE
+done < $INPUT
