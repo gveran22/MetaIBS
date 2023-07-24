ed617c8056229603600e5e2cae9de60058813c19
Salomé Carcy <shalom.carcy@gmail.com>
2023-07-23 20:32:29 -0400

    update metadata fetching Pozuelo

--- data/analysis-individual/Pozuelo-2015/00_Metadata-Pozuelo/README.md
+++ data/analysis-individual/Pozuelo-2015/00_Metadata-Pozuelo/README.md
@@ -1,4 +1,4 @@
 This directory contains metadata for the Pozuelo dataset.
-- `host_subtype.csv`: metadata table with IBS subtype, obtained from the [00_Metadata-Pozuelo.ipynb](../../../../scripts/analysis-individual/Pozuelo-2015/00_Metadata-Pozuelo.ipynb) notebook (need to get IBS subtype from BioSample "Description" slot on SRA website);
 - `Metadata-Pozuelo.csv`: final metadata table with essential information, cleaned up from `PozueloSraRunTable.txt` and merging with `host_subtype.csv`, using the [00_Metadata-Pozuelo.R](../../../../scripts/analysis-individual/Pozuelo-2015/00_Metadata-Pozuelo.R) script;
+- `Pozuelo_hostsubtype.csv`: metadata table with IBS subtype, obtained from the [00_Metadata-Pozuelo.ipynb](../../../../scripts/analysis-individual/Pozuelo-2015/00_Metadata-Pozuelo.ipynb) notebook (need to get IBS subtype from BioSample "Description" slot on SRA website);
 - `PozueloSraRunTable.csv`: metadata table downloaded from the [SRA Run Selector](https://www.ncbi.nlm.nih.gov/Traces/study/?acc=PRJNA268708&o=acc_s%3Aa) (accession number PRJNA268708).
--- scripts/analysis-individual/Pozuelo-2015/00_Metadata-Pozuelo.R
+++ scripts/analysis-individual/Pozuelo-2015/00_Metadata-Pozuelo.R
@@ -12,57 +12,56 @@
 library(tidyverse)
 
 # Data
-path <- "~/Projects/MetaIBS/scripts/analysis-individual/Pozuelo-2015"
+path <- "~/Projects/MetaIBS/data/analysis-individual/Pozuelo-2015"
 sraDF <- read.table(file.path(path, "00_Metadata-Pozuelo/PozueloSraRunTable.txt"), header = TRUE, sep = ",")
-subtypeDF <- read.csv(file.path(path, "00_Metadata-Pozuelo/host_subtype.csv"))
+subtypeDF <- read.csv(file.path(path, "00_Metadata-Pozuelo/Pozuelo_hostsubtype.csv"))
 
 
 #____________________________________________________________________
 # DATAFRAME CLEANUP
 
-# Keep relevant covariates
-sampledf <- sraDF[, c("Run", "Sample.Name", "Collection_Date")]
-rownames(sampledf) <- sampledf$Run
-
-sampledf$Sample.Name <- as.character(sampledf$Sample.Name)
-
-# Collection date
-sampledf$Collection <- "1st" # add column to specify if sample is from first or 2nd collection
-sampledf[gsub('.*(?=.{2}$)', '', sampledf$Sample.Name, perl=T) == '.2', 'Collection'] <- "2nd"
-
-# host_disease
-sampledf$host_disease <- "Healthy" # add column for host_disease
-sampledf[substr(sampledf$Sample.Name, 1, 2) == "MO", 'host_disease'] <- "IBS"
-
-# Sample.Name (host_ID)
-sampledf[, 'Sample.Name'] <- gsub("MO", "IBS", sampledf[, 'Sample.Name']) # replace 'MO' by 'IBS' in the sample.names
-sampledf$Sample.Name <- gsub("\\.2$", "", sampledf[, 'Sample.Name']) # remove the '.2' at the end of some of the sample.names (indicating it is the 2nd collection)
-
-colnames(sampledf) <- c("Run", "host_ID", "collection_date", "Collection", "host_disease")
-
-sampledf$sample_type <- "stool"
-sampledf$author <- "Pozuelo"
-sampledf$sequencing_tech <- "Illumina single-end"
-sampledf$variable_region <- "V4"
+# Cleanup SRA metadata
+sampledf <- sraDF %>%
+  dplyr::select(c("Run", "Sample.Name", "Collection_Date")) %>%
+  mutate(Sample.Name=as.character(Sample.Name),
+         Collection=ifelse(gsub('.*(?=.{2}$)', '', Sample.Name, perl=T) == '.2', "2nd", "1st"),
+         host_disease=ifelse(substr(Sample.Name, 1, 2) == "HC", "Healthy", "IBS"),
+         sample_type="stool",
+         author="Pozuelo",
+         sequencing_tech="Illumina single-end",
+         variable_region="V4") %>%
+  # remodel the Sample.Name
+  mutate(Sample.Name=gsub("MO", "IBS", Sample.Name),
+         Sample.Name=gsub("\\.2$", "", Sample.Name)) %>%
+  # rename columns
+  dplyr::rename(host_ID=Sample.Name,
+                collection_date=Collection_Date)
 
 # Join with subtypeDF
 sampledf <- subtypeDF %>%
-  select(Run, host_subtype) %>%
+  dplyr::select(Run, host_subtype) %>%
   left_join(sampledf, by="Run") %>%
   mutate(host_subtype = replace(host_subtype, !host_subtype %in% c("HC", "IBS-D", "IBS-C", "IBS-M"), "TO-DISCARD"),
          host_disease = replace(host_disease, !host_subtype %in% c("HC", "IBS-D", "IBS-C", "IBS-M"), "TO-DISCARD"))
-table(sampledf$host_disease) # sanity check
+table(sampledf$host_disease, useNA="ifany") # sanity check: we should have 88 HC and 185 IBS
 
 # Keep only healthy/IBS samples
 sampledf <- sampledf %>%
   filter(host_disease != "TO-DISCARD")
+rownames(sampledf) <- sampledf$Run
 
 
 #____________________________________________________________________
 # SAVE DATAFRAME 
 
-write.csv(sampledf, file.path(path, "00_Metadata-Pozuelo/Pozuelo-Metadata.csv"))
+write.csv(sampledf, file.path(path, "00_Metadata-Pozuelo/Metadata-Pozuelo.csv"))
 
-# Export list of files to download
-write.table(sampledf$Run, file.path(path, "download-Pozuelo-samples/list_files_pozuelo.txt"),
+# Export the list of Runs to download
+# Open the filereport downloaded from the ENA
+ena_table <- read.csv(file.path(path, "raw_fastq/filereport_read_run_PRJNA268708_tsv.txt"), header=T, sep="\t")
+table(ena_table$run_accession %in% sampledf$Run, useNA="ifany") # 273 samples of interest and 17 that we don't want to download
+ena_table <- ena_table[ena_table$run_accession %in% sampledf$Run,]
+dim(ena_table) # 273 rows
+# Export the list of links to download the Runs from ENA
+write.table(ena_table$fastq_ftp, file.path(path, "raw_fastq/list_files_pozuelo.txt"),
             sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
