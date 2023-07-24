b9de399d1f6819079c0808265b67b0acf4b692aa
Salomé Carcy <shalom.carcy@gmail.com>
2023-07-23 20:31:26 -0400

    Pozuelo: move download folder to data/

--- data/analysis-individual/Pozuelo-2015/raw_fastq/download_fastq_pozuelo.sh
+++ data/analysis-individual/Pozuelo-2015/raw_fastq/download_fastq_pozuelo.sh
@@ -5,9 +5,10 @@
 
 INPUT=list_files_pozuelo.txt
 
-for FILE in $(cat list_files_pozuelo.txt )
+for FILE in $(cat list_files_pozuelo.txt)
 do
 	echo "Run: $FILE"
 	#~/sratoolkit/bin/fastq-dump --origfmt -X 2 -Z $FILE
-	~/sratoolkit/bin/fastq-dump --origfmt --gzip $FILE
+	#~/sratoolkit/bin/fastq-dump --origfmt --gzip $FILE
+	wget $FILE
 done < $INPUT
