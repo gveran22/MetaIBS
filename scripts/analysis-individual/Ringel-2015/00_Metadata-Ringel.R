b3c14fc027c5753e5e78b8749e8ee9c26e02079c
Salomé Carcy <shalom.carcy@gmail.com>
2023-07-24 22:06:34 -0400

    Ringel: update metadata

--- scripts/analysis-individual/Ringel-2015/00_Metadata-Ringel.R
+++ scripts/analysis-individual/Ringel-2015/00_Metadata-Ringel.R
@@ -12,7 +12,7 @@
 library(tidyverse)
 
 # Data
-path <- "~/Projects/MetaIBS/scripts/analysis-individual/Ringel-2015"
+path <- "~/Projects/MetaIBS/data/analysis-individual/Ringel-2015"
 sraDF <- read.table(file.path(path, "00_Metadata-Ringel/RingelSraRunTable.txt"), header = TRUE, sep = ",")
 
 
@@ -22,6 +22,12 @@ sraDF <- read.table(file.path(path, "00_Metadata-Ringel/RingelSraRunTable.txt"),
 # Take a look at table
 colnames(sraDF)
 head(sraDF) # there isn't any covariates
+dim(sraDF) # there's double the number of samples we expect (2*76=152)
+table(sraDF$Bases) # every nb of bases appears twice, further proof that samples are duplicated
+table(sraDF[,c("LibrarySelection", "BioSample")]) # clearly showing samples are duplicated
+
+# Keep only half of the samples
+sraDF <- sraDF[sraDF$BioSample=="SAMN04272978",]
 
 # Keep relevant covariates
 sampledf <- data.frame(sraDF[,"Run"])
@@ -40,7 +46,21 @@ sampledf$variable_region <- "V1-V2"
 #____________________________________________________________________
 # SAVE DATAFRAME 
 
-write.csv(sampledf, file.path(path, "00_Metadata-Ringel/Ringel-Metadata.csv"))
+write.csv(sampledf, file.path(path, "00_Metadata-Ringel/Metadata-Ringel.csv"))
+
+# Export the list of Runs to download
+# Open the filereport downloaded from the ENA
+ena_table <- read.csv(file.path(path, "raw_fastq/filereport_read_run_PRJNA302437_tsv.txt"), header=T, sep="\t")
+table(ena_table$run_accession %in% sampledf$Run, useNA="ifany") # 76 samples of interest and 76 that we don't want to download
+ena_table <- ena_table[ena_table$run_accession %in% sampledf$Run,]
+dim(ena_table) # 76 rows
+# Export the list of links to download the Runs from ENA
+write.table(ena_table$fastq_ftp, file.path(path, "raw_fastq/list_files_ringel.txt"),
+            sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+
+
+
+
 
 # Export list of files to download
 write.table(sampledf$Run, file.path(path, "download-Ringel-samples/list_files_ringel.txt"),
